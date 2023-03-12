d308e73ca42b16c73d69801c6440f27e0b97e275
Dmitri <Dmtrgrvnk@gmail.com>
2023-03-13 00:53:50 +0300

    translate_to_en_bot=v1.2

--- core/lexicon/lexicon.py
+++ core/lexicon/lexicon.py
@@ -1,12 +1,7 @@
 LEXICON_EN: dict[str, str] = {
-    'start': "Hello! I'm a translator bot, click /help for more details.",
-    'help': "Type  <b>'en '</b>  before the message and I will translate it into English.\n" 
-            "Type  <b>'ru '</b>  before the message and I will translate it into Russian.\n"
-            "Type  <b>'de '</b>  before the message and I will translate it into Deutsche.",
-    'no_lang': "You didn't choose a language."
+    'start': "Hello! I'm a bot translator, send me a message and I'll translate it."
 }
 
 LEXICON_CMD_EN: dict[str, str] = {
     '/start': "start bot",
-    '/help': "description"
 }
